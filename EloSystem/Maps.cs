8dca28faaf41ffc3b8a282766952483052be8405
Turkey <spearzyy@gmail.com>
2021-04-07 23:35:23 +0100

    Add project files.

---
+++ EloSystem/Maps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EloSystem
+{
+    public class map
+    {
+        public map()
+        {
+            mapName = "empty";
+            maps = 0;
+        }
+
+        public map(string mapname, int ms)
+        {
+            mapName = mapname;
+            maps = ms;
+        }
+
+        public string mapName { get; set; }
+        public int maps { get; set; }
+    }
+}
